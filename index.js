e79edc94cc64192f0921f86554b48e3b35a8f14c
Alvaro Cabero <alvarocabero@gmail.com>
2021-11-03 18:33:18 +0100

    :arrow_up: Added index.js

---
+++ index.js
@@ -0,0 +1,18 @@
+require("dotenv").config();
+
+const http = require("http");
+
+const server = http.createServer();
+
+const port = process.env.SERVER_PORT_AVENTURA || 5000;
+
+server.listen(port, () => {
+  console.log(`Escuchando en el puerto ${port}`);
+});
+
+server.on("request", (request, response) => {
+  console.log("Uhl·la, ha llegado una request");
+  response.setHeader("Content-Type", "application/json");
+  response.write(JSON.stringify({ name: "Luis", age: 30 }));
+  response.end();
+});
