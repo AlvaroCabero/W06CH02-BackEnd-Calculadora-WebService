8443ca36b05e1bbfbb275d97e1141f08f931db50
Alvaro Cabero <alvarocabero@gmail.com>
2021-11-04 01:46:59 +0100

    :arrow_up: Added commander

--- index.js
+++ index.js
@@ -1,21 +1,32 @@
+const { program } = require("commander");
 require("dotenv").config();
+
 // const url = require("url");
+
+program.version("0.0.1");
+program.option("-p, --port <type>", "port number");
+
+program.parse(process.argv);
+
+const options = program.opts();
 
 const http = require("http");
 
 const server = http.createServer();
 
-const port = process.env.SERVER_PORT_MAIN || 5000;
+const port = options.port || process.env.SERVER_PORT_MAIN || 5000;
 
 server.listen(port, () => {
   console.log(`Escuchando en el puerto ${port}`);
 });
 
 server.on("request", (request, response) => {
-  const urlToCheck = `http://localhost:4500${request.url}`;
-  let value1 = 0;
-  let value2 = 0;
+  const urlToCheck = `http://localhost${request.url}`;
+  let value1 = "";
+  let value2 = "";
   let show = "";
+
+  let isOk = true;
   const urlSearchParams = new URL(urlToCheck).searchParams;
 
   console.log(urlSearchParams);
@@ -23,25 +34,44 @@ server.on("request", (request, response) => {
   if (request.url.includes("calculator")) {
     value1 = +urlSearchParams.get("a");
     value2 = +urlSearchParams.get("b");
-    console.log(value1);
-    console.log(value2);
-    if (Number.isNaN(value1) || Number.isNaN(value2)) {
+    // console.log(value1);
+    // console.log(value2);
+    if (
+      !value1 ||
+      !value2 ||
+      Number.isNaN(value1) ||
+      Number.isNaN(value2)
+      // value1.length === 0 ||
+      // value2.length === 0
+    ) {
+      console.log("error");
+      show = `<h1>Calculadora WebService</h1>
+<h2>ERROR 404</h2>
+<div>Please, set valid input  values<div/>
+`;
+      // response.statusCode = 404;
+      // response.end();
+      isOk = false;
+    } else {
       show = `<h1>Calculadora WebService</h1>
 <h2>Resultados</h2>
 <div>${value1} + ${value2} = ${value1 + value2}<div/>
 <div>${value1} - ${value2} = ${value1 - value2}<div/>
 <div>${value1} * ${value2} = ${value1 * value2}<div/>
-<div>${value1} / ${value2} = ${value1 / value2}<div/>`;
+<div>${value1} / ${value2} = ${(value1 / value2).toFixed(2)}<div/>`;
       response.statusCode = 200;
+      isOk = true;
+      console.log(isOk);
     }
   } else {
     console.log("error");
     show = `<h1>Calculadora WebService</h1>
-<h2>ERROR</h2>
-<div>Los valores deben ser de tipo numérico<div/>
+<h2>ERROR 404</h2>
+<div>Please, set valid input  values<div/>
 `;
-    response.statusCode = 404;
-    response.end();
+    // response.statusCode = 404;
+    // response.end();
+    isOk = false;
   }
 
   response.setHeader("Content-Type", "text/html");
